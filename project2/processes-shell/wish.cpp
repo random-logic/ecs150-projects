b045d11a8cfa420004022e0d6a104ac35adfe757
Andrew Jowe <andrewjowe@Andrews-MacBook-Pro.local>
2024-04-11 00:38:50 -0700

    progress

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <istream>
+#include <fstream>
+#include <unistd.h> // for getpid()
+#include <memory> // for unique_ptr
 
 using namespace std;
 
+void printErr() {
+    cerr << "An error has occurred\n";
+}
 
+// g++ -o wish wish.cpp -Wall -Werror
 int main(int argc, char* argv[]) {
-    // ??
+    unique_ptr<istream> in;
+    
+    if (argc == 1) {
+        // Use cin
+        in = make_unique<istream>(cin.rdbuf());
+    }
+    else if (argc == 2) {
+        // Use fin
+        in = make_unique<ifstream>(argv[1]);
+
+        if (!(*in)) {
+            printErr();
+            return 1;
+        }
+    }
+    else {
+        printErr();
+        return 1;
+    }
+    
+    while (true) {
+        cout << "wish> ";
+
+        string input;
+        getline(*in, input);
+
+        if (input == "exit") {
+            break;
+        }
+    }
+
+    return 0;
 }
