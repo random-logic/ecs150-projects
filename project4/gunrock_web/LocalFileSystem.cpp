aa84e50d474e65af5925526458d9d17525d3ad48
Andrew Jowe <andrewjowe@yahoo.com>
2024-05-24 20:05:01 -0700

    progress

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -6,6 +6,8 @@
 #include "LocalFileSystem.h"
 #include "ufs.h"
 
+#include <cstring>
+
 using namespace std;
 
 
@@ -14,29 +16,79 @@ LocalFileSystem::LocalFileSystem(Disk *disk) {
 }
 
 void LocalFileSystem::readSuperBlock(super_t *super) {
-
+  char buffer[UFS_BLOCK_SIZE];
+  this->disk->readBlock(0, buffer);
+  std::memcpy(super, buffer, sizeof(super_t));
 }
 
 int LocalFileSystem::lookup(int parentInodeNumber, string name) {
+  super_t super_block;
+  readSuperBlock(&super_block);
+
+  if (parentInodeNumber < 0 || parentInodeNumber >= super_block.num_inodes)
+    return -EINVALIDINODE;
+
+  // TODO: ??
+
   return 0;
 }
 
 int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
+  super_t super_block;
+  readSuperBlock(&super_block);
+
+  if (inodeNumber < 0 || inodeNumber >= super_block.num_inodes)
+    return -EINVALIDINODE;
+
+  // TODO: ??
+
   return 0;
 }
 
 int LocalFileSystem::read(int inodeNumber, void *buffer, int size) {
+  super_t super_block;
+  readSuperBlock(&super_block);
+
+  if (inodeNumber < 0 || inodeNumber >= super_block.num_inodes)
+    return -EINVALIDINODE;
+
+  // TODO: ??
+
   return 0;
 }
 
 int LocalFileSystem::create(int parentInodeNumber, int type, string name) {
+  super_t super_block;
+  readSuperBlock(&super_block);
+
+  if (parentInodeNumber < 0 || parentInodeNumber >= super_block.num_inodes)
+    return -EINVALIDINODE;
+
+  // TODO: ??
+
   return 0;
 }
 
 int LocalFileSystem::write(int inodeNumber, const void *buffer, int size) {
+  super_t super_block;
+  readSuperBlock(&super_block);
+
+  if (inodeNumber < 0 || inodeNumber >= super_block.num_inodes)
+    return -EINVALIDINODE;
+
+  // TODO: ??
+
   return 0;
 }
 
 int LocalFileSystem::unlink(int parentInodeNumber, string name) {
+  super_t super_block;
+  readSuperBlock(&super_block);
+
+  if (parentInodeNumber < 0 || parentInodeNumber >= super_block.num_inodes)
+    return -EINVALIDINODE;
+
+  // TODO: ??
+
   return 0;
 }
