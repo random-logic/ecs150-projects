a0febace61eb927030daeb787fa131db1db7cdc8
Andrew <andrewjowe@yahoo.com>
2024-05-26 12:22:26 -0700

    some progress

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -28,7 +28,13 @@ int LocalFileSystem::lookup(int parentInodeNumber, string name) {
   if (parentInodeNumber < 0 || parentInodeNumber >= super_block.num_inodes)
     return -EINVALIDINODE;
 
-  // TODO: ??
+  inode_t theParentInode;
+  this->stat(parentInodeNumber, &theParentInode);
+
+  if (theParentInode.type != UFS_DIRECTORY)
+    return -EINVALIDINODE;
+
+  // TODO: implement ??
 
   return 0;
 }
@@ -40,7 +46,13 @@ int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
   if (inodeNumber < 0 || inodeNumber >= super_block.num_inodes)
     return -EINVALIDINODE;
 
-  // TODO: ??
+  int theBlockToRead = super_block.inode_region_addr + inodeNumber * sizeof(inode_t) / UFS_BLOCK_SIZE;
+  int theInodeOffset = inodeNumber * sizeof(inode_t) % UFS_BLOCK_SIZE;
+
+  char buffer[UFS_BLOCK_SIZE];
+  this->disk->readBlock(theBlockToRead, buffer);
+
+  std::memcpy(inode, buffer + theInodeOffset, sizeof(inode_t));
 
   return 0;
 }
@@ -91,4 +103,68 @@ int LocalFileSystem::unlink(int parentInodeNumber, string name) {
   // TODO: ??
 
   return 0;
+}
+
+bool LocalFileSystem::diskHasSpace(super_t *super, int numInodesNeeded, int numDataBytesNeeded, int numDataBlocksNeeded=0) {
+  // ??
+}
+
+void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap) {  
+  int theTotalSizeOfBitmap = super->inode_bitmap_len * UFS_BLOCK_SIZE;
+  
+  for (int i = 0; i < super->inode_bitmap_len; ++i) {
+    int theBlockToRead = super->inode_bitmap_addr + i;
+    auto theBufferStart = inodeBitmap + i * UFS_BLOCK_SIZE;
+    disk->readBlock(theBlockToRead, theBufferStart);
+  }
+}
+
+void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
+  int theTotalSizeOfBitmap = super->inode_bitmap_len * UFS_BLOCK_SIZE;
+  
+  for (int i = 0; i < super->inode_bitmap_len; ++i) {
+    int theBlockToRead = super->inode_bitmap_addr + i;
+    auto theBufferStart = inodeBitmap + i * UFS_BLOCK_SIZE;
+    disk->writeBlock(theBlockToRead, theBufferStart);
+  }
+}
+
+void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap) {
+  int theTotalSizeOfBitmap = super->data_bitmap_len * UFS_BLOCK_SIZE;
+
+  for (int i = 0; i < super->inode_bitmap_len; ++i) {
+    int theBlockToRead = super->data_bitmap_addr + i;
+    auto theBufferStart = dataBitmap + i * UFS_BLOCK_SIZE;
+    disk->readBlock(theBlockToRead, theBufferStart);
+  }
+}
+
+void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap) {
+  int theTotalSizeOfBitmap = super->data_bitmap_len * UFS_BLOCK_SIZE;
+
+  for (int i = 0; i < super->inode_bitmap_len; ++i) {
+    int theBlockToRead = super->data_bitmap_addr + i;
+    auto theBufferStart = dataBitmap + i * UFS_BLOCK_SIZE;
+    disk->writeBlock(theBlockToRead, theBufferStart);
+  }
+}
+
+void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes) {
+  int theTotalSizeOfBitmap = super->inode_region_len * UFS_BLOCK_SIZE;
+
+  for (int i = 0; i < super->inode_region_len; ++i) {
+    int theBlockToRead = super->inode_region_addr + i;
+    auto theBufferStart = inodes + i * UFS_BLOCK_SIZE;
+    disk->readBlock(theBlockToRead, theBufferStart);
+  }
+}
+
+void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes) {
+  int theTotalSizeOfBitmap = super->inode_region_len * UFS_BLOCK_SIZE;
+
+  for (int i = 0; i < super->inode_region_len; ++i) {
+    int theBlockToRead = super->inode_region_addr + i;
+    auto theBufferStart = inodes + i * UFS_BLOCK_SIZE;
+    disk->writeBlock(theBlockToRead, theBufferStart);
+  }
 }
