ce42f162d2200792aab09323709bf74d79e6f100
Andrew Jowe <andrewjowe@Andrews-MacBook-Pro.local>
2024-06-02 14:30:59 -0700

    ls util

--- project4/gunrock_web/ds3ls.cpp
+++ project4/gunrock_web/ds3ls.cpp
@@ -14,4 +14,33 @@ int main(int argc, char *argv[]) {
     cout << argv[0] << ": diskImageFile" << endl;
     return 1;
   }
+
+  string theNameOfTheDiskImage(argv[1]);
+  Disk theDisk(theNameOfTheDiskImage, UFS_BLOCK_SIZE);
+  LocalFileSystem theLocalFileSystem(&theDisk);
+
+  super_t theSuperBlock;
+  theLocalFileSystem.readSuperBlock(&theSuperBlock);
+
+  printContentsOfAllDirectories(theSuperBlock.data_region_addr, theLocalFileSystem);
+}
+
+void printContentsOfAllDirectories(const int theInodeNumberToRead, LocalFileSystem &theLocalFileSystem) {
+  inode_t theInode;
+  theLocalFileSystem.stat(theInodeNumberToRead, &theInode);
+  
+  const int theNumberOfEntries = theInode.size / sizeof(dir_ent_t);
+  dir_ent_t theEntries[theNumberOfEntries];
+  theLocalFileSystem.read(theInodeNumberToRead, theEntries, theNumberOfEntries * sizeof(dir_ent_t));
+
+  for (dir_ent_t entry : theEntries) {
+    cout << entry.name << endl;
+  }
+
+  for (dir_ent_t entry : theEntries) {
+    if (entry.name == ".." || entry.name == ".")
+      continue;
+
+    printContentsOfAllDirectories(entry.inum, theLocalFileSystem);
+  }
 }
