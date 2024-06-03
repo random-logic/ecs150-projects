4955f8d7ead6797e18e60be11bd5927a9b49f5eb
Andrew Jowe <andrewjowe@Andrews-MacBook-Pro.local>
2024-06-02 22:43:24 -0700

    work ds3ls

--- project4/gunrock_web/ds3cat.cpp
+++ project4/gunrock_web/ds3cat.cpp
@@ -15,4 +15,29 @@ int main(int argc, char *argv[]) {
     cout << argv[0] << ": diskImageFile inodeNumber" << endl;
     return 1;
   }
+
+  Disk theDisk(argv[1], UFS_BLOCK_SIZE);
+  LocalFileSystem theLocalFileSystem(&theDisk);
+
+  super_t theSuperBlock;
+  theLocalFileSystem.readSuperBlock(&theSuperBlock);
+
+  int theInodeNumber = atoi(argv[2]);
+  inode_t theInode;
+  theLocalFileSystem.stat(theInodeNumber, &theInode);
+
+  cout << "File blocks" << endl;
+  for (int i = 0; i < (theInode.size + UFS_BLOCK_SIZE - 1) / UFS_BLOCK_SIZE; ++i) {
+    cout << theInode.direct[i] << endl;
+  }
+  cout << endl;
+
+  cout << "File data" << endl;
+  char fileData[MAX_FILE_SIZE];
+  int bytesRead = theLocalFileSystem.read(theInodeNumber, fileData, MAX_FILE_SIZE);
+  
+  for (int i = 0; i < bytesRead; ++i)
+    cout << fileData[i];
+
+  cout << endl;
 }
